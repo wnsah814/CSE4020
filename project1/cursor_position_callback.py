2a16dd65a452e07191b0b28ad6061792ffb18155
wnsah814 <sjmskm@gmail.com>
2023-05-14 20:46:16 +0900

    modify detecting mouse moving direction

--- project1/cursor_position_callback.py
+++ project1/cursor_position_callback.py
@@ -7,7 +7,7 @@ def cursor_position_callback(window, xpos, ypos):
     # orbit
     if glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS:
         drag_start_x, drag_start_y = drag.get_position()
-        dx, dy = xpos - drag_start_x, drag_start_y - ypos 
+        dx, dy = drag_start_x - xpos , drag_start_y - ypos
         drag.set_position(xpos, ypos)
 
         SCALER = 0.1
@@ -22,11 +22,11 @@ def cursor_position_callback(window, xpos, ypos):
     # pan
     elif glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS:
         drag_start_x, drag_start_y = drag.get_position()
-        dx, dy = xpos - drag_start_x, drag_start_y - ypos
+        dx, dy = drag_start_x - xpos , drag_start_y - ypos
         drag.set_position(xpos, ypos)
         V = global_cam.get_view_matrix()
         V = glm.inverse(V)
         if not dx == 0:
             global_cam.pan += V[0].xyz * 0.01 * dx
         if not dy == 0:
-            global_cam.pan += V[1].xyz * 0.01 * dy
+            global_cam.pan -= V[1].xyz * 0.01 * dy
--- project2/cursor_position_callback.py
+++ project2/cursor_position_callback.py
@@ -8,7 +8,6 @@ def cursor_position_callback(window, xpos, ypos):
     if glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS:
         drag_start_x, drag_start_y = drag.get_position()
         dx, dy = drag_start_x - xpos , drag_start_y - ypos
-        # dx, dy = xpos - drag_start_x, drag_start_y - ypos 
         drag.set_position(xpos, ypos)
 
         SCALER = 0.1
@@ -24,11 +23,10 @@ def cursor_position_callback(window, xpos, ypos):
     elif glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS:
         drag_start_x, drag_start_y = drag.get_position()
         dx, dy = drag_start_x - xpos , drag_start_y - ypos
-        # dx, dy = xpos - drag_start_x, drag_start_y - ypos
         drag.set_position(xpos, ypos)
         V = global_cam.get_view_matrix()
         V = glm.inverse(V)
         if not dx == 0:
             global_cam.pan += V[0].xyz * 0.01 * dx
         if not dy == 0:
-            global_cam.pan += V[1].xyz * 0.01 * dy
+            global_cam.pan -= V[1].xyz * 0.01 * dy
